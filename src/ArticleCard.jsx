d5d32389fd29cbc2ba5568fd62497de55719a4b5
ellsbee <katebroniahurley@gmail.com>
2024-06-18 16:21:30 +0100

    completes task 06-view-associated-comments

--- src/ArticleCard.jsx
+++ src/ArticleCard.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function ArticleCard({article}) {
-    console.log(article)
     return(
         <ul className="article-card">
             <Link to={`/article/${article.article_id}`}>
             <img src={article.article_img_url}/>
-            <h3>{article.title}</h3>
+            <h1>{article.title}</h1>
             </Link>
         </ul>
     )
--- src/SingleArticleCard.jsx
+++ src/SingleArticleCard.jsx
@@ -1,11 +1,13 @@
-import { getArticleById } from "../Utils/api";
+import { getArticleById, getAllArticleComments } from "../Utils/api";
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom' 
 import './SingleArticleStyle.css';
+import CommentCard from "./CommentCard";
 
 function SingleArticleCard() {
     const {article_id} = useParams();
     const [article, setArticle] = useState(null)
+    const [comments, setComments] = useState([])
 
     useEffect(() => {
         getArticleById(article_id)
@@ -14,6 +16,13 @@ function SingleArticleCard() {
     })
     .catch((err) => {
         console.log('there was an error retreiving the article', err)
+    })
+    getAllArticleComments(article_id)
+    .then(data => {
+        setComments(data.comments)
+    })
+    .catch(err => {
+        console.log('there was an error retrieving the comments', err)
     })
     }, [article_id])
 
@@ -26,6 +35,13 @@ function SingleArticleCard() {
             <h1>{article.title}</h1>
             <img src={article.article_img_url} alt={article.title}/>
             <p>{article.body}</p>
+            <div className="article-comments">
+                <h2>Comments</h2>
+                {comments.map((comment) => {
+                return <CommentCard comment={comment} key={comment.comment_id}/>
+            })}
+
+            </div>
         </div>
     )
 }
